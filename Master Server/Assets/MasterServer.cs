a128484346f6a253dfeb297abefd1b220472544d
QuaintShanty <quaint.shanty@gmail.com>
2016-03-03 03:48:06 -0500

    Started working on the server script.

---
+++ Master Server/Assets/MasterServer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Networking;
+
+public class MasterServer : NetworkManager {
+
+	void Awake() {
+        // Initializing the Transport Layer with no arguments (default settings)
+        NetworkTransport.Init();
+
+        // Configuration Channels
+        ConnectionConfig config = new ConnectionConfig();
+        int reliableChanId = config.AddChannel(QosType.Reliable);
+        int unreliableChanId = config.AddChannel(QosType.Unreliable);
+
+
+    }
+}
